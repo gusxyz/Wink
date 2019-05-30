99e391393346a96c57f7874969cc2e76c43d55b1
August Eymann <august.eymann@gmail.com>
2019-05-29 21:15:07 -0700

    Server Barebones 💀

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
-const app = require('express')();
+const express = require('express');
 const volleyball = require('volleyball');
+const db = require ('./db/connection')
+//  App Initialization
+const app = express();
 
+// Database 
+const urls = db.get('urls');
+
+//  Middleware
 app.use(volleyball);
+app.use(express.json());
 
-
-app.get('/', (req,res) => {
+//  Routing
+app.get('/api/v1/', (req,res) => {
     res.json({
         'message': 'The Wink ;) API is set for blast off! 🚀'
     })
+});
+
+app.get('/api/v1/url', (req,res) => {
+    urls.find({}).then(urls => {
+        res.json(posts);
+    }) 
 });
 
 app.listen(3000, () => console.log('Wink is listening on port 3000!'));
